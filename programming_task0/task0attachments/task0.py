a58494a2c90b53096362ac38ff6eced7f6ff312d
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-02-24 21:15:29 +0800

    chore: initialize pt0

---
+++ programming_task0/task0attachments/task0.py
@@ -0,0 +1,15 @@
+from cs145lib.task0 import Channel, node_main
+
+
+def sender(channel: Channel, sentence: str) -> None:
+    # TODO implement this
+    ...
+
+
+def receiver(channel: Channel) -> str:
+    # TODO implement this
+    return ""
+
+
+if __name__ == "__main__":
+    node_main(sender=sender, receiver=receiver)
