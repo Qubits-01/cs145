fdea840ed1dbcb778498481597dae853883fb615
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-02-29 09:06:18 +0800

    dev: improve the correctness of the algorithm

--- programming_task1/task1attachments/task1.py
+++ programming_task1/task1attachments/task1.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from cs145lib.task1 import Channel, node_main
 
 word_key_map = {}  # Exclusively for the sender function only.
@@ -18,15 +19,15 @@ with open("corpus.txt", "r") as corpus_file:
 
 
 def sender(channel: Channel, sentence: str) -> None:
-    repetition_number = 40
+    start_signal_repetition_number = 10
+    message_repetition_number = 10
 
     def send_bit_n_times(bit: int, repetition_number: int) -> None:
         for _ in range(repetition_number):
             channel.send(bit)
 
     # Signal the start of the communication.
-    for _ in range(repetition_number):
-        channel.send(1)
+    send_bit_n_times(1, start_signal_repetition_number)
 
     # Remove "." from the sentence.
     sentence = sentence[:-1]
@@ -35,7 +36,7 @@ def sender(channel: Channel, sentence: str) -> None:
     for word in sentence.split():
         bits = word_key_map[word.lower()]
         for bit in bits:
-            send_bit_n_times(int(bit), repetition_number)
+            send_bit_n_times(int(bit), message_repetition_number)
 
     # Signal the end of the communication.
     # Four most significant bits (1111) signify the end of the communication.
@@ -45,61 +46,72 @@ def sender(channel: Channel, sentence: str) -> None:
     # For instance, the last word in the corpus "zulus" has a 16-bit
     # key "1110001011111101" (notice that the first four MSBs are "1110").
     for _ in range(4):
-        send_bit_n_times(1, repetition_number)
+        send_bit_n_times(1, message_repetition_number)
 
 
 def receiver(channel: Channel) -> str:
-    buffer_size = 40
-    buffer_queue = []  # Size: 10
-    buffer_read_cooldown = 0
-    max_bits = 3500
-    raw_bits_read = 0
+    bit_flip_probability = 0.1  # 10% bit flip probability
+    offset = 1
+    start_signal_repetition_number = 10
+    start_signal_buffer_queue = []
+    message_repetition_number = 10
+    message_buffer_queue = []
+    message_max_bits = 3500  # If the message end indicator was not read properly.
+    raw_message_bits_read = 0
 
-    def add_bit_to_buffer(bit: int) -> None:
-        nonlocal buffer_read_cooldown
-        nonlocal raw_bits_read
-
-        if len(buffer_queue) == buffer_size:
+    def add_bit_to_buffer(
+        bit: int,
+        buffer_queue: list = message_buffer_queue,
+        max_len: int = message_repetition_number,
+    ) -> None:
+        if len(buffer_queue) == max_len:
             buffer_queue.pop(0)
 
         buffer_queue.append(bit)
-        buffer_read_cooldown += 1
-        raw_bits_read += 1
 
-    def read_buffer_identity(buffer_queue: list) -> int:
-        nonlocal buffer_read_cooldown
+    ### Read the buffer queue and return the identity of the buffer.
+    ### The identity of the buffer is determined by the number of ones
+    ### and zeroes in the buffer.
+    def get_ones_zeroes_count(buffer_queue: list) -> Tuple[int, int]:
+        n_ones = buffer_queue.count(1)
+        return n_ones, len(buffer_queue) - n_ones
 
-        if buffer_read_cooldown < buffer_size:
-            return 0
-
-        count_ones = buffer_queue.count(1)
-        count_zeros = buffer_queue.count(0)
-
-        buffer_read_cooldown = 0
-
-        return 1 if count_ones > count_zeros else 0
+    def read_message_buffer_identity(
+        buffer_queue: list = message_buffer_queue,
+    ) -> int:
+        n_ones, n_zeroes = get_ones_zeroes_count(buffer_queue)
+        return 1 if n_ones > n_zeroes else 0
 
     # Wait for the start signal.
     while True:
-        add_bit_to_buffer(channel.get())
-        read_bit = read_buffer_identity(buffer_queue)
+        add_bit_to_buffer(
+            channel.get(),
+            start_signal_buffer_queue,
+            start_signal_repetition_number,
+        )
 
-        if read_bit == 1:
-            break
+        if len(start_signal_buffer_queue) == start_signal_repetition_number:
+            n_ones, _ = get_ones_zeroes_count(start_signal_buffer_queue)
+            required_ones = int(
+                ((1 - bit_flip_probability) * start_signal_repetition_number) - offset
+            )
+
+            if n_ones >= required_ones:
+                break
 
     # Read the "bits" of the message.
     sentence = []
     while True:
-        if raw_bits_read > max_bits:
+        if raw_message_bits_read > message_max_bits:
             break
 
         curr_bit_string = ""
         for _ in range(16):
-            # Fill the buffer queue with [buffer_size] bits.
-            for _ in range(buffer_size):
+            # Fill the buffer queue with [message_repetition_number] bits.
+            for _ in range(message_repetition_number):
                 add_bit_to_buffer(channel.get())
 
-            curr_bit_string += str(read_buffer_identity(buffer_queue))
+            curr_bit_string += str(read_message_buffer_identity())
             if curr_bit_string == "1111":
                 break
 
