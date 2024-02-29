94dad76e4aa0b71392e3d87b54ea5b253fedab70
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-02-29 16:54:17 +0800

    dev: change buffer algo approach

--- programming_task1/task1attachments/task1.py
+++ programming_task1/task1attachments/task1.py
@@ -1,3 +1,4 @@
+from sys import stderr
 from typing import Tuple
 from cs145lib.task1 import Channel, node_main
 
@@ -19,12 +20,18 @@ with open("corpus.txt", "r") as corpus_file:
 
 
 def sender(channel: Channel, sentence: str) -> None:
-    start_signal_repetition_number = 10
-    message_repetition_number = 10
+    start_signal_repetition_number = 30
+    message_repetition_number = 30
+
+    # print("Sending start signal...", file=stderr)
 
     def send_bit_n_times(bit: int, repetition_number: int) -> None:
         for _ in range(repetition_number):
             channel.send(bit)
+
+    # print("Start signal sent.", file=stderr)
+
+    # print("Sending message...", file=stderr)
 
     # Signal the start of the communication.
     send_bit_n_times(1, start_signal_repetition_number)
@@ -38,6 +45,10 @@ def sender(channel: Channel, sentence: str) -> None:
         for bit in bits:
             send_bit_n_times(int(bit), message_repetition_number)
 
+    # print("Message sent.", file=stderr)
+
+    # print("Sending end signal...", file=stderr)
+
     # Signal the end of the communication.
     # Four most significant bits (1111) signify the end of the communication.
     # This works because 111 is the highest possible MSBs for a 16-bit key
@@ -48,15 +59,19 @@ def sender(channel: Channel, sentence: str) -> None:
     for _ in range(4):
         send_bit_n_times(1, message_repetition_number)
 
+    # print("✅ End signal sent.", file=stderr)
+
 
 def receiver(channel: Channel) -> str:
     bit_flip_probability = 0.1  # 10% bit flip probability
-    offset = 1
+    offset = 5
     start_signal_repetition_number = 30
     start_signal_buffer_queue = []
-    message_repetition_number = 20
+    message_repetition_number = 30
     message_buffer_queue = []
-    message_max_bits = 3500  # If the message end indicator was not read properly.
+    start_signal_max_bits = 1500  # If the start signal was not read properly.
+    raw_start_signal_bits_read = 0
+    message_max_bits = 3000  # If the message end indicator was not read properly.
     raw_message_bits_read = 0
 
     def add_bit_to_buffer(
@@ -82,8 +97,13 @@ def receiver(channel: Channel) -> str:
         n_ones, n_zeroes = get_ones_zeroes_count(buffer_queue)
         return 1 if n_ones > n_zeroes else 0
 
+    # print("Waiting for start signal...", file=stderr)
+
     # Wait for the start signal.
     while True:
+        if raw_start_signal_bits_read > start_signal_max_bits:
+            exit("Start signal not received.")
+
         add_bit_to_buffer(
             channel.get(),
             start_signal_buffer_queue,
@@ -99,6 +119,11 @@ def receiver(channel: Channel) -> str:
             if n_ones >= required_ones:
                 break
 
+        raw_start_signal_bits_read += 1
+
+    # print("Start signal received.", file=stderr)
+
+    # print("Reading message...", file=stderr)
     # Read the "bits" of the message.
     sentence = []
     while True:
@@ -120,6 +145,11 @@ def receiver(channel: Channel) -> str:
             break
 
         sentence.append(word)
+
+        raw_message_bits_read += 1
+
+    # print("✅ Message received.", file=stderr)
+    # print(f'message: {(" ".join(sentence) + ".").capitalize()}', file=stderr)
 
     return (" ".join(sentence) + ".").capitalize()
 
--- programming_task1/task1attachments/test1.py
+++ programming_task1/task1attachments/test1.py
@@ -2,25 +2,41 @@
 
 import argparse
 import subprocess
-from math import log2 
+from math import log2
 import os
-import time  
+import time
 
 DEFAULT_NUM_TESTS = 100
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog="test0.py", description="Tests your Programming Task 1 (PT 1) solution.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="test0.py", description="Tests your Programming Task 1 (PT 1) solution."
+    )
 
-    parser.add_argument("-s", "--seed", default=int(0xC0DEBABE), help="Random seed for test program (integer; default: decimal equivalent of 0xC0DEBABE).", type=int)
-    parser.add_argument("-n", "--num-tests", default=100, help="Number of tests to run (integer; default: 100).", type=int)
+    parser.add_argument(
+        "-s",
+        "--seed",
+        default=int(0xC0DEBABE),
+        help="Random seed for test program (integer; default: decimal equivalent of 0xC0DEBABE).",
+        type=int,
+    )
+    parser.add_argument(
+        "-n",
+        "--num-tests",
+        default=100,
+        help="Number of tests to run (integer; default: 100).",
+        type=int,
+    )
 
     args = parser.parse_args()
 
     seed_arg, num_tests_arg = args.seed, args.num_tests
 
     if not os.path.exists("task1.py"):
-        print(f'ERROR\n\
-            \tAre you in the right folder?')
+        print(
+            f"ERROR\n\
+            \tAre you in the right folder?"
+        )
         exit()
 
     outputs = []
@@ -32,19 +48,31 @@ if __name__ == '__main__':
         command = f"python3 -m cs145lib.task1.make_sentence --seed {i + seed_arg} | \
                 python3 -m cs145lib.task1.test --seed {i + seed_arg} \
                 python3 task1.py"
-        
+
+        # print(f"[test0.py] Running command: {command}")
+
         # Time the process
         start_time = time.time()
-    
+
         # Run the command and capture the output
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
         # Read the output and errors
         output, error = process.communicate()
 
+        # print(i)
+        # print(error)
+        # print(output)
+
         # Calculate elapsed time
         times.append(time.time() - start_time)
-        
+
         # Store output and error
         outputs.append(output)
         errors.append(error)
@@ -64,32 +92,38 @@ if __name__ == '__main__':
         received_str = " ".join(received_msg)
         sent_str = " ".join(sent_msg)
         if received_msg != sent_msg or t >= 10:
-            print(f'ERROR FOR SEED {i + seed_arg}:\n\
+            print(
+                f"ERROR FOR SEED {i + seed_arg}:\n\
                 \tBits: {bits}\n\
                 \tTime: {t} s\n\
                 \tSent: {sent_str}\n\
-                \tReceived: {received_str}')
+                \tReceived: {received_str}"
+            )
             test_errors += 1
-            
+
         total_bits += bits
-        
+
         if t >= 10:
             time_errors += 1
-    
+
     # Display results
-    print(f'TESTS\n\
+    print(
+        f"TESTS\n\
             \tSeed: {seed_arg}\n\
             \tTests performed: {len(outputs)}\n\
             \tTests timed out: {time_errors}\n\
-            \tFailed tests: {test_errors}')
-    
+            \tFailed tests: {test_errors}"
+    )
+
     # End if all tests failed - otherwise we encounter div-by-0 errors
     if test_errors == len(outputs):
         exit()
 
     # Compute score
     avg = total_bits / len(outputs)
-    x = log2(total_bits if num_tests_arg == DEFAULT_NUM_TESTS else avg * DEFAULT_NUM_TESTS)
+    x = log2(
+        total_bits if num_tests_arg == DEFAULT_NUM_TESTS else avg * DEFAULT_NUM_TESTS
+    )
     if time_errors >= 3:
         score = 0
     elif x > 21.99:
@@ -105,15 +139,19 @@ if __name__ == '__main__':
     if score < 0:
         score = 0
 
-    print(f'PERFORMANCE\n\
+    print(
+        f"PERFORMANCE\n\
             \tTotal bits: {total_bits}\n\
             \tAverage bits per message: {avg}\n\
             \tX: {x}\n\
-            \tScore: {score}')
+            \tScore: {score}"
+    )
 
     if num_tests_arg != 100:
-        print(f'NOTE\n\
+        print(
+            f"NOTE\n\
             \tNumber of tests is not 100.\n\
             \tScore here might not accurately reflect correctness of solution:\n\
             \t- Average bits per message was used to determine X.\n\
-            \t- Penalties were scaled according to number of tests ran.')
+            \t- Penalties were scaled according to number of tests ran."
+        )
